8b80a880cde24f22c44719b5faac74f11a058e27
Zhi GUO <zguoau@connect.ust.hk>
2022-03-18 22:01:52 +0800

    111

--- boy.cpp
+++ boy.cpp
@@ -31,6 +31,41 @@ ModelerView* createSampleModel(int x, int y, int w, int h, char* label)
     return new Boy(x, y, w, h, label);
 }
 
+GLuint loadBMP_custom(const char* imagepath) {
+	unsigned char header[54]; // Each BMP file begins by a 54-bytes header
+	unsigned int dataPos;     // Position in the file where the actual data begins
+	unsigned int width, height;
+	unsigned int imageSize;   // = width*height*3
+	unsigned char* data;
+	FILE* file = fopen(imagepath, "rb");
+	if (!file)
+	{
+		return 0;
+	}
+	if (fread(header, 1, 54, file) != 54) { // If not 54 bytes read : problem
+		return false;
+	}
+	if (header[0] != 'B' || header[1] != 'M') {
+		return 0;
+	}
+	dataPos = *(int*)&(header[0x0A]);
+	imageSize = *(int*)&(header[0x22]);
+	width = *(int*)&(header[0x12]);
+	height = *(int*)&(header[0x16]);
+	if (imageSize == 0)    imageSize = width * height * 3;
+	if (dataPos == 0)      dataPos = 54;
+	data = new unsigned char[imageSize];
+	fread(data, 1, imageSize, file);
+	fclose(file);
+	GLuint textureID;
+	glGenTextures(1, &textureID);
+	glBindTexture(GL_TEXTURE_2D, textureID);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	return textureID;
+}
+
 // We are going to override (is that the right word?) the draw()
 // method of ModelerView to draw out SampleModel
 void Boy::draw()
@@ -76,13 +111,9 @@ void Boy::draw()
 	glLightfv(GL_LIGHT0, GL_DIFFUSE, lightDiffuse0);
 	glLightfv(GL_LIGHT1, GL_POSITION, lightPosition1);
 	glLightfv(GL_LIGHT1, GL_DIFFUSE, lightDiffuse1);
-	// draw the floor
-	/*setAmbientColor(.0f, .0f, .0f);
-	setDiffuseColor(COLOR_BLUE);
-	glPushMatrix();
-	glTranslated(-5, 0, -5);
-	drawBox(10, 0.01f, 10);
-	glPopMatrix();*/
+	
+	GLuint image = loadBMP_custom("ferry.bmp");
+	
 
 	// draw the model
 	setAmbientColor(.0f, .0f, .0f);
